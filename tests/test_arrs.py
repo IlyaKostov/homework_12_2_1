01c213a13edd775531c4f18d363b5ae083539591
Kostov Ilya <kick1991@mail.ru>
2023-06-07 20:55:22 +0500

    use fixture

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,10 +3,15 @@ import pytest
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+@pytest.fixture
+def array_fixture():
+    return [1, 2, 3, 4]
+
+
+def test_get(array_fixture):
+    assert arrs.get(array_fixture, 1, "test") == 2
     # assert arrs.get([], 0, "test") == "test"
-    assert arrs.get([1, 2, 3], -4, "test") == "test"
+    assert arrs.get(array_fixture, -5, "test") == "test"
 
 
 def test_get_index_error():
@@ -14,9 +19,9 @@ def test_get_index_error():
         arrs.get([], 0, "test")
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(array_fixture):
+    assert arrs.my_slice(array_fixture, 1, 3) == [2, 3]
+    assert arrs.my_slice(array_fixture, 1) == [2, 3, 4]
     assert arrs.my_slice([]) == []
-    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
-    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
+    assert arrs.my_slice(array_fixture, -2) == [3, 4]
+    assert arrs.my_slice(array_fixture, -5) == [1, 2, 3, 4]
